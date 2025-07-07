f65953223d38c590f95aca3f6544f4ba7656f022
Divs-dev <contact@divssfdc.com>
2025-07-07 15:05:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const pool = new Pool({
 app.post('/insert', async (req, res) => {
   const { Id, Name, Field_Name__c, Action__c, Account__c, Lead__c, Funding__c, New_Value__c, Old_Value__c, CreatedDate, user__c } = req.body;
   try {
+    console.log('> '+req.body);
     const result = await pool.query(
       'INSERT INTO funding_flow (Id, Name, Field_Name__c, Action__c, Account__c, Lead__c, Funding__c, New_Value__c, Old_Value__c, CreatedDate, user__c) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
       [Id, Name, Field_Name__c, Action__c, Account__c, Lead__c, Funding__c, New_Value__c, Old_Value__c, CreatedDate, user__c]
